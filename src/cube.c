7431e96411a7690665661f5e4bbdb67c0034ee58
iluvpy <miomanns265@gmail.com>
2021-11-02 22:58:03 +0100

    added simple cube rotating

--- src/cube.c
+++ src/cube.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 int main(void)
 {
     GLFWwindow* window;
@@ -10,7 +11,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Cube", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -27,20 +28,80 @@ int main(void)
 	}
 
 	printf("%s\n", glGetString(GL_VERSION));
-
+	glEnable(GL_DEPTH_TEST);
+	float angle = 0.0f;
+	float increment = 1.0f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		angle += increment;
+		glLoadIdentity();
+		glRotatef(angle, 1.0f, 1.0f, 0.0f);
 
-		glBegin(GL_TRIANGLES);
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glVertex2f( 0.0f,  0.5f);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f( 0.5f, -0.5f);
+		glBegin(GL_POLYGON);
+		
+		// face 1 (front)
+		glColor3f(  1.0f, 0.0f, 0.0f);
+		glVertex3f(-0.5f,  0.5f, 0.5f);
+		glVertex3f(-0.5f, -0.5f, 0.5f);
+		glVertex3f( 0.5f, -0.5f, 0.5f);
+		glVertex3f( 0.5f,  0.5f, 0.5f);
+
+		glEnd();
+		glBegin(GL_POLYGON);
+
+		// face 2 (left)
+		glColor3f(  0.0f,  1.0f,  0.0f);
+		glVertex3f(-0.5f,  0.5f,  0.5f);
+		glVertex3f(-0.5f, -0.5f,  0.5f);
+		glVertex3f(-0.5f, -0.5f, -0.5f);
+		glVertex3f(-0.5f,  0.5f, -0.5f);
+
+		glEnd();
+		glBegin(GL_POLYGON);
+
+		// face 3 (right)
+		glColor3f(  0.0f,  0.0f,  1.0f);
+		glVertex3f( 0.5f,  0.5f,  0.5f);
+		glVertex3f( 0.5f, -0.5f,  0.5f);
+		glVertex3f( 0.5f, -0.5f, -0.5f);
+		glVertex3f( 0.5f,  0.5f, -0.5f);
+
+		glEnd();
+		glBegin(GL_POLYGON);
+
+		// face 4 (back)
+		glColor3f(  0.5f,  0.5f,  0.5f);
+		glVertex3f(-0.5f,  0.5f, -0.5f);
+		glVertex3f(-0.5f, -0.5f, -0.5f);
+		glVertex3f( 0.5f, -0.5f, -0.5f);
+		glVertex3f( 0.5f,  0.5f, -0.5f);
+
+		glEnd();
+		glBegin(GL_POLYGON);
+
+		// face 5 (top)
+		glColor3f(  1.0f,  1.0f,  1.0f);
+		glVertex3f(-0.5f,  0.5f, -0.5f);
+		glVertex3f(-0.5f,  0.5f,  0.5f);
+		glVertex3f( 0.5f,  0.5f,  0.5f);
+		glVertex3f( 0.5f,  0.5f, -0.5f);
+
+		glEnd();
+		glBegin(GL_POLYGON);
+
+		// face 6 (bottom)
+		glColor3f(  0.8f,  0.1f,  0.8f);
+		glVertex3f(-0.5f, -0.5f,  0.5f);
+		glVertex3f( 0.5f, -0.5f,  0.5f);
+		glVertex3f( 0.5f, -0.5f, -0.5f);
+		glVertex3f(-0.5f, -0.5f, -0.5f);
+
 		glEnd();
 
+	
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
